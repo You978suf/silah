dc83e711ee10533fd376c4922be5dacfd99628b1
Ibrahim2b <s139337@student.squ.edu.om>
2024-05-06 20:08:32 +0400

    search

--- search2.php
+++ search2.php
@@ -2,48 +2,34 @@
 $sname = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "silahinfo";
+$dbname = "silah";
 
-// Create connection
 $conn = new mysqli($sname, $user, $pass, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve team member selection from the form
-$teamMemberId = $_POST["teamMember"];
+$loginEmail = $_POST["loginEmail"];
 
-// Prepare SQL query based on selected team member
-$sql = "";
-if ($teamMemberId == 0) {
-    $sql = "select * from teammembers where name = 'Yousuf alshaaili'";
-} else if ($teamMemberId == 1) {
-    $sql = "select * from teammembers where name = 'Ibrahim Altoubi'";
-} else if ($teamMemberId == 2) {
-    $sql = "select * from teammembers where name = 'Qusay Alshueili'";
-} else {
-    echo "Invalid selection";
-}
+$sql = "SELECT * FROM logedin WHERE email = '$loginEmail'";
 
-
-// Execute SQL query
 $result = mysqli_query($conn, $sql);
 
-// Check if any rows are returned
 if (mysqli_num_rows($result) > 0) {
-    // Loop through each row and display the information
+    echo "<table border='1'>";
+    $row = mysqli_fetch_assoc($result);
+
+    mysqli_data_seek($result, 0);
+
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<table border='1'>";
         echo "<tr><td>Name:</td><td>{$row['name']}</td></tr>";
-        echo "<tr><td>Education:</td><td>{$row['education']}</td></tr>";
-        echo "<tr><td>Skills:</td><td>{$row['skills']}</td></tr>";
-        echo "<tr><td>Information:</td><td>{$row['informations']}</td></tr>";
-        echo "</table>";
+        echo "<tr><td>Email:</td><td>{$row['email']}</td></tr>";
+        echo "<tr><td>Password:</td><td>{$row['passwords']}</td></tr>";
     }
+    echo "</table>";
 } else {
-    echo "No results found";
+    echo "You are not logged in with this account";
 }
 
 mysqli_close($conn);
