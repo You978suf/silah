8d497f7f8c702f09031c52665934155da5ee1a88
Y0u5uf <s137312@student.squ.edu.om>
2024-05-07 12:29:33 +0400

    .

---
+++ delete.php
@@ -0,0 +1,40 @@
+<?php
+$sname = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "silah";
+
+
+$conn = new mysqli($sname, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$contactName = $_POST["contactName"];
+
+
+$sql = "DELETE FROM submissions WHERE name = '$contactName'";
+
+$result = mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result) > 0) {
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<table border='1'>";
+        echo "<tr><td>Name:</td><td>{$row['name']}</td></tr>";
+        echo "<tr><td>Email:</td><td>{$row['email']}</td></tr>";
+        echo "<tr><td>Phone Number:</td><td>{$row['phonenumber']}</td></tr>";
+        echo "<tr><td>Help Type:</td><td>{$row['helptype']}</td></tr>";
+        echo "<tr><td>Message:</td><td>{$row['message']}</td></tr>";
+        echo "</table> \n";
+        echo "Deleted successufuly"
+    }
+} else {
+    echo "There is no previous contact with this name";
+}
+
+mysqli_close($conn);
+?>
